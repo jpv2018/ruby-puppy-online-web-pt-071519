f185c82086cee4ccf9d06f69b74db6701a520695
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-24 20:35:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Add your code here
+class Dog
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def save
+    @@all.push self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def print_all
+    @@all.each do |dog|
+      puts dog
+    end
+  end
